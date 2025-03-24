2b2c78aaafdba4cd1dcd6b86aea13474d4bda4c8
oznurtopcu <topcuoznur121@gmail.com>
2025-03-24 23:03:33 +0300

    convertDecimalToBinary task finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,36 @@
 package org.example;
 
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        checkForPalindrome("Was it a car or a cat I saw ?");
     }
 
     public static boolean checkForPalindrome(String str) {
 
         String newString = "";
-        str = str.toLowerCase().replaceAll("[^a-zA-Z\\s]", "");
-
+        //Locale.ROOT ifadesini dil bağımsız dönüşüm yapmak için kullandık, yoksa test geçmiyor
+        str = str.toLowerCase(Locale.ROOT).replaceAll("[^a-zA-Z\\s]", "");
+        str = str.replaceAll(" ", "");
         for(int i=0;i<str.length();i++) {
             newString = str.charAt(i) + newString;
-        }
 
+        }
         return newString.equals(str);
     }
 
+    public static String convertDecimalToBinary(int number) {
+        String binary = "";
+        while(number>0) {
+            int i = number % 2;
+            binary = String.valueOf(i) + binary;
+            number = number/2;
+        }
 
+        return binary;
+    }
 
 }
