f9c9aae7f7745e0df0f86e94f7c4e5431acb142a
oznurtopcu <topcuoznur121@gmail.com>
2025-03-24 22:19:17 +0300

    checkForPalindrome task finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,4 +6,18 @@ public class Main {
         System.out.println("Hello world!");
     }
 
+    public static boolean checkForPalindrome(String str) {
+
+        String newString = "";
+        str = str.toLowerCase().replaceAll("[^a-zA-Z\\s]", "");
+
+        for(int i=0;i<str.length();i++) {
+            newString = str.charAt(i) + newString;
+        }
+
+        return newString.equals(str);
+    }
+
+
+
 }
